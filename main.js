78772b2a1e9604123e8f403f42119129da4a09f1
AgusAstuDev <agusastudillo.dev@gmail.com>
2024-10-20 19:23:47 -0300

    Se ha hecho el ejercicio 2

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Script ejercicio 2
+import { sumar } from "./scripts/ejercicio2";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+// Ejercicio 2
+// Declaro variables a y b
+let a = 5;
+let b = 10;
+// Declaro variable c con la función sumar
+let c = sumar(a, b);
+// Muestro por pantalla
+console.log(c);
---
+++ scripts/ejercicio2.js
@@ -0,0 +1,2 @@
+// Creamos una arrow function para sumar a y b
+export const sumar = (a, b) => a + b;
